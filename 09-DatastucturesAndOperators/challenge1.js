4d65fe11ecb15024a68acf2152498ceb9b493baf
Abhishek Singh <as9284608@gmail.com>
2022-11-18 07:27:07 +0530

    challenge

--- 09-DatastucturesAndOperators/challenge1.js
+++ 09-DatastucturesAndOperators/challenge1.js
@@ -37,10 +37,16 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+
+  printGoals(...scorers) {
+    console.log(scorers);
+    console.log(scorers.length);
+  },
 };
 //task 1:
-const [players1, players2] = [...game.players];
-// console.log(players1, players2);
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
 
 //task 2:
 const [gk, ...fieldPlayers] = players1;
@@ -48,9 +54,21 @@ console.log(gk);
 
 //tasks 3:
 const allPlayers = [...players1, ...players2];
-// console.log(allPlayers);
+console.log(allPlayers);
 
 //task 4:
-
 const players1Final = ["thiago", "Cuantinho", "perisic", ...players1];
-console.log(players1Final);
+// console.log(players1Final);
+
+//task 5:
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+//task 6:
+game.printGoals("Davis", "Muller", "Lewandowski", "kimmich");
+game.printGoals(...game.scored);
+
+//task 7:
+team1 < team2 && console.log("team 1 is more likely to win");
+team1 > team2 && console.log("team 2 is more likely to win");
