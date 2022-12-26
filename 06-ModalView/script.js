8c16840258f7beea3a05fa53eff076d04ff5fd97
Abhishek Singh <as9284608@gmail.com>
2022-12-26 16:52:09 +0530

    modal window,  handeling classes and keyboad event

--- 06-ModalView/script.js
+++ 06-ModalView/script.js
@@ -1,2 +1,31 @@
 'use strict';
 //revising from beginning
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+const exitModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i of btnOpenModal) {
+  i.addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', exitModal);
+overlay.addEventListener('click', exitModal);
+
+//handeling keypress event
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    exitModal();
+  }
+});
