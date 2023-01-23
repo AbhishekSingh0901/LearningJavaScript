e498aa1173f4d94f9edbeda2c9abf3bc23f026dd
Abhishek Singh <as9284608@gmail.com>
2023-01-23 16:53:46 +0530

    challenge 3 based on maps

--- 09-DatastucturesAndOperators/challenges.js
+++ 09-DatastucturesAndOperators/challenges.js
@@ -1,37 +1,37 @@
 const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
   players: [
     [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
     ],
     [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
     ],
   ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
   odds: {
     team1: 1.33,
     x: 3.25,
@@ -57,7 +57,7 @@ const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
 //task 4:
-const players1Final = ["thiago", "Cuantinho", "perisic", ...players1];
+const players1Final = ['thiago', 'Cuantinho', 'perisic', ...players1];
 // console.log(players1Final);
 
 //task 5:
@@ -66,12 +66,12 @@ const { team1, x: draw, team2 } = game.odds;
 console.log(team1, draw, team2);
 
 //task 6:
-game.printGoals("Davis", "Muller", "Lewandowski", "kimmich");
+game.printGoals('Davis', 'Muller', 'Lewandowski', 'kimmich');
 game.printGoals(...game.scored);
 
 //task 7:
-team1 < team2 && console.log("team 1 is more likely to win");
-team1 > team2 && console.log("team 2 is more likely to win");
+team1 < team2 && console.log('team 1 is more likely to win');
+team1 > team2 && console.log('team 2 is more likely to win');
 
 /*Coding Challenge #2
 
@@ -122,6 +122,62 @@ console.log(sum / count);
 
 //task 3:
 for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`odd of ${teamStr} ${odd}`);
+}
+
+/*Coding Challenge #3
+
+Let's continue with our football betting app! This time, we have a map called 
+'gameEvents' (see below) with a log of the events that happened during the 
+game. The values are the events themselves, and the keys are the minutes in which 
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no 
+duplicates)
+
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
+
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL */
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+//1
+const values = gameEvents.values();
+console.log([...values]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+//4
+for (let [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
 }
