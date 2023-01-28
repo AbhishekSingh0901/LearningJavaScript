8736c46c306634e6ff2ffce6bbae6a9af720f5e7
Abhishek Singh <as9284608@gmail.com>
2023-01-28 17:06:23 +0530

    reduce and challenge question

--- 11-WorkingWithArrays/script.js
+++ 11-WorkingWithArrays/script.js
@@ -82,6 +82,12 @@ const diplayMovements = function (movements) {
 
 diplayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
 //using forEach() and Map() methods
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -94,7 +100,8 @@ const createUserNames = function (accs) {
 };
 
 createUserNames(accounts);
-console.log(account1, account2, account3, account4);
+// console.log(account1, account2, account3, account4);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -204,7 +211,7 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 //*My own practice:
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+console.log(movements);
 // const newMsvi = movements.map(function (movement, i) {
 //   if (movement > 0) {
 //     return `Movement ${i + 1} You deposited ${movement}`;
@@ -230,3 +237,62 @@ const newMD = movements.map(
 );
 
 console.log(newMD);
+
+//working with filter() method (learning):
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
+
+//working with reduce() method (Learing):
+const balance = movements.reduce(
+  (acc, cur) => acc + cur,
+  0 /*this is initial accumulator value*/
+);
+
+console.log(balance);
+
+//Maximum Value
+const max = movements.reduce(
+  (
+    acc,
+    mov /*{
+  if (acc > mov) {
+    return acc;
+  } else return (acc = mov);
+}*/
+  ) => (acc > mov ? acc : (acc = mov)),
+  movements[0]
+);
+
+console.log(max);
+
+//*Challenge 2
+/*Coding Challenge #2
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
+dog ages to human ages and calculate the average age of the dogs in their study.
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is 
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+humanAge = 16 + dogAge * 4
+
+2. Exclude all dogs that are less than 18 human years old (which is the same as 
+keeping dogs that are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know 
+from other challenges how we calculate averages �)
+
+4. Run the function for both test datasets
+
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+*/
