eaa1f803a38e74e4270ea3fee5ab203754b0655a
Abhishek Singh <as9284608@gmail.com>
2023-02-05 21:50:18 +0530

    operations with Dates

--- 12-NumbersDatesAndTimers/script.js
+++ 12-NumbersDatesAndTimers/script.js
@@ -24,8 +24,8 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-02-01T23:36:17.929Z',
+    '2023-02-04T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -83,6 +83,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementsDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago!`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
 const displayMovements = function (account, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -94,11 +110,8 @@ const displayMovements = function (account, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementsDate(date);
+    console.log(displayDate);
 
     const html = `
       <div class="movements__row">
@@ -167,11 +180,11 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
-//dat/month/year:
+// //dat/month/year:
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -406,18 +419,26 @@ console.log(13n / 3n); //4n always return integer value
 // console.log(new Date(0));
 // console.log(3 * 24 * 60 * 60 * 1000);
 
-//Working with date:
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(future);
-console.log(future.getFullYear());
-console.log(future.getDate());
-console.log(future.getMonth());
-console.log(future.getDay()); //Day of the week sunday is 0
-console.log(future.toISOString());
-console.log(future.getTime());
+// //Working with date:
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getDate());
+// console.log(future.getMonth());
+// console.log(future.getDay()); //Day of the week sunday is 0
+// console.log(future.toISOString());
+// console.log(future.getTime());
 
-console.log(new Date(2142237180000));
+// console.log(new Date(2142237180000));
 
-console.log(Date.now());
-future.setFullYear(2040);
-console.log(future);
+// console.log(Date.now());
+// future.setFullYear(2040);
+// console.log(future);
+
+//Operations With Dates:
+
+const future = new Date(2010, 10, 19, 15, 23);
+console.log(+future);
+
+// const day1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 19));
+// console.log(day1);
