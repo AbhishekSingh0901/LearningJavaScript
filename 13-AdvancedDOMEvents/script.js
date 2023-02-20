4e094a2aadeafeb7ade5c059946d9b92288d6712
Abhishek Singh <as9284608@gmail.com>
2023-02-20 15:38:35 +0530

    learning intersection observer API

--- 13-AdvancedDOMEvents/script.js
+++ 13-AdvancedDOMEvents/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const header = document.querySelector('.header');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -142,13 +143,64 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 //*sticky navigation
 
 //window scroll should be avoided usually
-const initialCorrds = section1.getBoundingClientRect();
-console.log(initialCorrds);
+// const initialCorrds = section1.getBoundingClientRect();
+// console.log(initialCorrds);
 
-window.addEventListener('scroll', function (e) {
-  if (window.scrollY > initialCorrds.top) {
-    nav.classList.add('sticky');
-  } else nav.classList.remove('sticky');
+// window.addEventListener('scroll', function (e) {
+//   if (window.scrollY > initialCorrds.top) {
+//     nav.classList.add('sticky');
+//   } else nav.classList.remove('sticky');
+// });
+
+// stickyNavigation: Intersection Observer Api:
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  if (entry.isIntersecting) nav.classList.remove('sticky');
+};
+
+const headerobserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerobserver.observe(header);
+
+//* Reveal Sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionOberver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.2,
+});
+allSections.forEach(function (section) {
+  sectionOberver.observe(section);
+  section.classList.add('section--hidden');
 });
 //////////////////////////////////////////////////////////////////
 // 1) Selecting,reating, and Deleting Elements:
