89faac390c597b809c207d198727baaf9585e9fa
Abhishek Singh <as9284608@gmail.com>
2023-02-23 16:21:59 +0530

    object.create and challenge 2

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -139,6 +139,29 @@ PersonCL.sayhi();
 // console.log(account.latest);
 // console.log((account.latest = 500));
 // console.log(account.movements);
+
+//* Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(`${this.fullName}'s age is ${2023 - this.birthyear}`);
+  },
+
+  init(fullName, birthyear) {
+    this.fullName = fullName;
+    this.birthyear = birthyear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.fullName = 'steven';
+steven.birthyear = 2000;
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', '2002');
+sarah.calcAge();
 ////////////////////////////////////////////////////////////////////////////////////
 
 // Coding Challenge #1
@@ -180,3 +203,51 @@ PersonCL.sayhi();
 // car1.accelerate();
 // car1.brake();
 // car1.brake();
+
+//Coding Challenge 2:
+
+// Your tasks:
+
+// 1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+
+// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+// by 1.6)
+
+// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+// converts it to km/h before storing the value, by multiplying the input by 1.6)
+
+// 4. Create a new car and experiment with the 'accelerate' and 'brake'
+// methods, and with the getter and setter.
+
+// Test data:
+// § Data car 1: 'Ford' going at 120 km/h
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    console.log(`speed: ${(this.speed += 10)}km/h`);
+  }
+
+  brake() {
+    console.log(`speed: ${(this.speed -= 5)}km/h`);
+  }
+
+  get speedUs() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford.speedUs);
+ford.accelerate();
+ford.brake();
+ford.speedUs = 80;
+console.log(ford.speed);
