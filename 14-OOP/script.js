13ffec3cdc96c6b0b784d73b550e1df7224decf8
Abhishek Singh <as9284608@gmail.com>
2023-02-23 15:33:18 +0530

    getter, setters and statiic methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
 //Contructor function
-const Person = function (firstname, birthyear) {
-  //Instance properties
-  this.firstName = firstname;
+const Person = function (fullName, birthyear) {
+  // Instance properties
+  this.fullName = fullName;
   this.birthyear = birthyear;
 
-  //! BAD PRACTICE
-  //You should never to this,
-  // this.calcAge = function () {
-  //   console.log(2023 - this.birthyear);
-  // };
+  // ! BAD PRACTICE
+  // You should never to this,
+  this.calcAge = function () {
+    console.log(2023 - this.birthyear);
+  };
 };
 
 //Prototypes: each and every function in JS automatically has a property called prototype
@@ -26,6 +26,12 @@ const matlida = new Person('matilda', 2000);
 // console.log(jonas);
 // console.log(matlida);
 
+// Person.hey = function () {
+//   console.log(`Hey There 😒`);
+// };
+
+// Person.hey();
+// console.log(Person);
 // jonas.calcAge();
 //1. a new empty object is created
 //2. function is called, this = object
@@ -40,7 +46,7 @@ const matlida = new Person('matilda', 2000);
 // console.log(jonas.__proto__);
 // console.log(jonas.__proto__ === Person.prototype);
 
-Person.prototype.species = 'Homo Sapiens';
+// Person.prototype.species = 'Homo Sapiens';
 // console.log(jonas, matlida);
 // console.log(jonas.species, matlida.species); //homo Sapiens
 // console.log(jonas.hasOwnProperty('firstName'));
@@ -51,15 +57,15 @@ Person.prototype.species = 'Homo Sapiens';
 // console.log(jonas.__proto__);
 // console.log(jonas.__proto__.__proto__);
 // console.log(jonas.__proto__.__proto__.__proto__);
-console.dir(Person.prototype.constructor);
-const arr = [2, 3, 5, 4, 55, 8, 6, 55, 3]; //new Array = []
+// console.dir(Person.prototype.constructor);
+// const arr = [2, 3, 5, 4, 55, 8, 6, 55, 3]; //new Array = []
 // console.log(arr.__proto__ === Array.prototype);
 // console.log(arr.__proto__.__proto__);
 
 //this is generallly not a good idea: DONT do it Practically!
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
 // console.log(arr.unique());
 
@@ -70,44 +76,69 @@ Array.prototype.unique = function () {
 
 //ES6 Classes
 
+//Insatance methods
 class PersonCL {
-  constructor(name, birthyear) {
-    this.name = name;
+  constructor(fullName, birthyear) {
+    this.fullName = fullName;
     this.birthyear = birthyear;
   }
 
   calcAge() {
-    console.log(`${this.firstName}'s age is ${2023 - this.birthyear}`);
+    console.log(`${this.fullName}'s age is ${2023 - this.birthyear}`);
+  }
+
+  get age() {
+    return 2023 - this.birthyear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static sayhi() {
+    console.log(`Hey There 😒`);
   }
 }
 
 const JohnDoe = new PersonCL('John Doe', 1986);
-
+// console.log(JohnDoe);
+// console.log(JohnDoe.age);
+// JohnDoe.calcAge();
 // JohnDoe.calcAge();
 
 PersonCL.prototype.greet = function () {
-  console.log(`hey ${this.name}`);
+  console.log(`hey ${this._fullName}`);
 };
 
 JohnDoe.greet();
+PersonCL.sayhi();
+// JohnDoe.sayhi();
+
+// const walter = new PersonCL('walter', 1965);
+// console.log(walter);
 
 //Getters and Setter:
-const account = {
-  owner: 'Jonas',
-  movements: [100, 200, 456, 842],
+// const account = {
+//   owner: 'Jonas',
+//   movements: [100, 200, 456, 842],
 
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
 
-  set latest(mov) {
-    this.movements.push(mov);
-  },
-};
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
 
-console.log(account.latest);
-console.log((account.latest = 500));
-console.log(account.movements);
+// console.log(account.latest);
+// console.log((account.latest = 500));
+// console.log(account.movements);
 ////////////////////////////////////////////////////////////////////////////////////
 
 // Coding Challenge #1
