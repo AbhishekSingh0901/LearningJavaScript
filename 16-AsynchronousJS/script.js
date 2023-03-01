9803e5e34b8f3cd3ba31bad0cea98dc2f4a56602
Abhishek Singh <as9284608@gmail.com>
2023-03-01 21:32:04 +0530

    callBack hell

--- 16-AsynchronousJS/script.js
+++ 16-AsynchronousJS/script.js
@@ -4,9 +4,57 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     // console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     // console.log(data);
+//     // console.log(data.flags.svg);
+//     const html = `
+//   <article class="country">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(2)} Mil people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(2)} Mil people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
 
   request.addEventListener('load', function () {
@@ -14,25 +62,40 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
     // console.log(data.flags.svg);
-    const html = `
-  <article class="country">
-    <img class="country__img" src="${data.flags.svg}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(2)} Mil people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.name}</p>
-    </div>
-  </article>`;
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    //Get Neighbour country:
+
+    const neighbour = data.borders[0];
+    if (!neighbour) return;
+    const nieghbourRequest = new XMLHttpRequest();
+    nieghbourRequest.open(
+      'GET',
+      `https://restcountries.com/v2/alpha/${neighbour}`
+    );
+    nieghbourRequest.send();
+    nieghbourRequest.addEventListener('load', function () {
+      const datanighobur = JSON.parse(this.responseText);
+      renderCountry(datanighobur, 'neighbour');
+    });
   });
 };
 
-getCountryData('india');
-getCountryData('usa');
-getCountryData('portugal');
+getCountryDataAndNeighbour('usa');
+
+//Another simpler Example:
+setTimeout(() => {
+  console.log('1 Sec passed');
+  setTimeout(() => {
+    console.log('1 Sec passed');
+    setTimeout(() => {
+      console.log('1 Sec passed');
+      setTimeout(() => {
+        console.log('1 Sec passed');
+        setTimeout(() => {
+          console.log('1 Sec passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
